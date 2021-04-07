32ece3a7f31290384ca7e36db1c7a82524f736ce
Georgi Maksimov <bin4ev@gmail.com>
2021-04-07 18:15:33 +0300

    Update notification.js

--- client/src/utils/notification.js
+++ client/src/utils/notification.js
@@ -5,6 +5,5 @@ return(
    !info? <div class="alert alert-dark" style={{display:'none'}} role="alert">{info} </div>
     :<div class="alert alert-dark" style={{display:'block'}} role="alert">{info} </div> 
 )
-
 }
 export default notification;
